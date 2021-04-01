ddc7eb8be46f8ab7a8ea2c9c7aca0fb39c3f7eab
dalaowai <nkabox8@gmail.com>
2021-04-01 14:38:04 +0300

    forms

--- taskmanager/main/views.py
+++ taskmanager/main/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
+from .forms import CustomerForm
 
 
-def  index(request):
+def index(request):
     return render(request, 'main/index.html')
 
 
-def  about(request):
+def about(request):
     return render(request, 'main/about.html')
 
 
 def  singin(request):
-    return render(request, 'main/singin.html')
+
+    form = CustomerForm()
+    
+    if request.method == 'POST':
+        form = CustomerForm(request.POST)
+        if form.is_valid():
+            form.save()
+            
+    return render(request, 'main/singin.html', {'form':form})
