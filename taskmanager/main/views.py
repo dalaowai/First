6427658caf511aee8ac8331fedd94142c922d116
dalaowai <nkabox8@gmail.com>
2021-04-12 14:31:04 +0300

    ups

--- taskmanager/main/views.py
+++ taskmanager/main/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
 
+@login_required(login_url='login')
 def index(request):
     return render(request, 'main/index.html')
 
